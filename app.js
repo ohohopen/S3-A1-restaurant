96a63b8fab0e6a55704bf8b90e37b6c5d1a8c1c2
ohohopen <ohohopen@gmail.com>
2022-04-02 18:01:20 +0800

    路由器 & mongooseDB 連線

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const router = require('./routes')
+require('./mongoose/mongoose')
 app.use(express.static('public'))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(router)
 app.listen(port, () => {
   console.log('app is connecting.')
 })
