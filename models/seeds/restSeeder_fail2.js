7e6d4c142d92921a1877d09ea1ead8ea0c6308d8
ohohopen <ohohopen@gmail.com>
2022-04-06 08:14:17 +0800

    錯誤之疑問

---
+++ models/seeds/restSeeder_fail2.js
@@ -0,0 +1,58 @@
+const db = require("../../config/mongoose");
+const restaurant_json = require("../../public/data/restaurant.json").results;
+const restLen = restaurant_json.length;
+const bcrypt = require("bcryptjs");
+const Restaurant = require("../restaurants");
+const User = require("../users");
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+const SEED_USERS = [
+	{
+		name: "USER1",
+		email: "user1@example.com",
+		password: "12345678",
+		restaurantIndex: [1, 2, 3],
+	},
+	{
+		name: "USER2",
+		email: "user2@example.com",
+		password: "12345678",
+		restaurantIndex: [4, 5, 6],
+	},
+];
+db.once("open", () => {
+	return Promise.all(
+		Array.from(SEED_USERS, (item) => {
+			bcrypt
+				.genSalt(10)
+				.then((salt) => bcrypt.hash(item.password, salt))
+				.then((hash) =>
+					User.create({
+						name: item.name,
+						email: item.email,
+						password: hash,
+						restaurantIndex: item.restaurantIndex,
+					})
+				)
+				.then((user) => {
+					const userId = user._id;
+					//選出餐廳序號和使用者相符的餐廳
+					let restArr = restaurant_json.filter((item) =>
+						user.restaurantIndex.includes(item.id)
+					);
+					//將餐廳們塞入userId, 使各筆資料屬於特定使用者
+					restArr.forEach((item) => (item.userId = userId));
+					Array.from(restArr, (item) => {
+						Restaurant.create(item);
+					});
+				})
+				.then(() => {
+					console.log("done.");
+				});
+		})
+	).then(() => {
+		console.log("這一部份比Array.form先被執行，導致直接exit了");
+		process.exit();
+	});
+});
