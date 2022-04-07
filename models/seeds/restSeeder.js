1a17bd5ce08483a9fd5dd1556299e1da96486896
ohohopen <ohohopen@gmail.com>
2022-04-07 14:57:40 +0800

    重構種子修正

--- models/seeds/restSeeder.js
+++ models/seeds/restSeeder.js
@@ -22,38 +22,38 @@ const SEED_USERS = [
 	},
 ];
 db.once("open", () => {
-	Array.from(SEED_USERS, (item) => {
-		bcrypt
-			.genSalt(10)
-			.then((salt) => bcrypt.hash(item.password, salt))
-			.then((hash) =>
-				User.create({
-					name: item.name,
-					email: item.email,
-					password: hash,
-					restaurantIndex: item.restaurantIndex,
-				})
-			)
-			.then((user) => {
-				const userId = user._id;
-				//選出餐廳序號和使用者相符的餐廳
-				let restArr = restaurant_json.filter((item) =>
-					user.restaurantIndex.includes(item.id)
-				);
-				//將餐廳們塞入userId, 使各筆資料屬於特定使用者
-				restArr.forEach((item) => (item.userId = userId));
-				return Promise.all(
-					Array.from(restArr, (item) => {
-						Restaurant.create(item);
+	Promise.all(
+		Array.from(SEED_USERS, (item) => {
+			return bcrypt
+				.genSalt(10)
+				.then((salt) => bcrypt.hash(item.password, salt))
+				.then((hash) =>
+					User.create({
+						name: item.name,
+						email: item.email,
+						password: hash,
+						restaurantIndex: item.restaurantIndex,
 					})
-				);
-			})
-			.then(() => {
-				console.log("done.");
-			});
-		//為什麼不能寫exit()???????
-		//process.exit();
-	});
+				)
+				.then((user) => {
+					const userId = user._id;
+					//選出餐廳序號和使用者相符的餐廳
+					let restArr = restaurant_json.filter((item) =>
+						user.restaurantIndex.includes(item.id)
+					);
+					//將餐廳們塞入userId, 使各筆資料屬於特定使用者
+					restArr.forEach((item) => (item.userId = userId));
+					return Promise.all(
+						Array.from(restArr, (item) => {
+							return Restaurant.create(item);
+						})
+					);
+				})
+				.then(() => {
+					console.log("done.");
+				});
+		})
+	).then(() => process.exit());
 
 	console.log("seeding is ok.");
 });
